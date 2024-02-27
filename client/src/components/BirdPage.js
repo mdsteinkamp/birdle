eaf132ac346eefc8d0eee759ca1421ec56d286ad
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-02-26 21:16:18 -0800

    BirdPage google img search returning first image link and rendering to page

--- client/src/components/BirdPage.js
+++ client/src/components/BirdPage.js
@@ -1,31 +1,35 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function BirdPage({ bird, onClickNext }) {
+  const [image, setImage] = useState(null)
 
   useEffect(() => {
-    async function findBirdImage() {
+    async function getImages() {
       try {
-        const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyB63JcEgZiiMGD_svwC8lA0IqNFvtZC0Eg&cx=e2e6b1e2c14314732:omuauf_lfve&searchType=image&q=${bird.name}`)
+        const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyB63JcEgZiiMGD_svwC8lA0IqNFvtZC0Eg&cx=e2e6b1e2c14314732&searchType=image&q=${bird.name}`)
 
-        const image = await response.json()
-        console.log(image)
+        const images = await response.json()
+        console.log(images)
+        setImage(images.items[0].link)
       } catch (error) {
         console.log(error)
       }
-      }
-  })
+    }
+    getImages()
+  }, [bird])
 
   function handleNextBird() {
     onClickNext()
 
   }
   console.log(bird)
+  console.log(image)
 
   return(
     (bird.images.length < 1) ? handleNextBird() :
     <div>
       <h2>{bird.name}</h2>
-      <img src={bird.images[0]} width={500} aspect-ratio={1 / 1} alt="bird?" />
+      <img src={image} width={500} aspect-ratio={1 / 1} alt="bird?" />
       <button onClick={handleNextBird}>Next bird</button>
 
     </div>
