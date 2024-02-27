4ca2a6e73fca78e341e9c8443cd6d188eb055a66
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-02-26 17:58:33 -0800

    handlenextbird updated with callback to enqueue bird

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,8 +7,7 @@ const birds = new Queue
 
 export default function App() {
   const [birdsList, setBirdsList] = useState(null)
-  const [nextBird, setNextBird] = useState(null)
-
+  const [visibleBird, setVisibleBird] = useState(null)
 
   async function findBird(setBird) {
       const id = Math.floor(Math.random() * (1069)) + 1
@@ -23,10 +22,7 @@ export default function App() {
           findBird()
         } else {
           const bird = await response.json()
-          console.log(bird)
           setBird(bird)
-          return nextBird
-          // return birds
           setBirdsList(birds)
         }
       } catch(error) {
@@ -35,31 +31,26 @@ export default function App() {
   }
 
   useEffect(() => {
+    setVisibleBird(birds.first)
     for(let i = 0; i < 5; i++) {
       findBird(bird => birds.enqueue(bird))
     }
   }, [])
 
-  // const nums = [1, 2, 3, 4, 5, 6]
-  // console.log(nums.filter(num => num < 4))
-
   function handleNextBird() {
     birds.dequeue()
     setBirdsList(birds)
-    findBird()
+    findBird(bird => birds.enqueue(bird))
   }
 
   console.log(birds)
   console.log(birds.first)
 
   return (
-    (!birds) ? null : 
+    (birds.length < 5) ? null : 
     <div className="App">
       <h1>Welcome to the BIRDLE</h1>
-      {/* <BirdPage bird={birdsList.first.data} onClick={handleClick} /> */}
-      {/* <h2>{birds.first.data.name}</h2>
-      <img src={birds.first.data.images[0]} width={500} aspect-ratio={1 / 1} alt="bird?" />
-      <button onClick={handleNextBird}>Next bird</button> */}
+      <BirdPage bird={birds.first} onClick={handleNextBird} />
     </div>
   );
 }
