0398c6b217af9c71e8c61a74f1e9656deaff3c09
Matthew Steinkamp <mdsteinkamp@gmail.com>
2024-03-12 20:27:35 -0700

    filtering bird in App to send only images, name, recordings

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -24,27 +24,28 @@ export default function App() {
           findBird(bird => birds.enqueue(bird))
         } else {
           const bird = await response.json()
+          const allowed = ["name", "images", "recordings"]
+          const filteredBird = Object.fromEntries(
+            Object.entries(bird).filter((([k, v]) => allowed.includes(k))
+          ))
+          console.log(filteredBird)
           setBird(bird)
           setBirdsList(birds)
           console.log(birds.length)
           birds.length === 1? getFirstImage() : getImages()
-          // setVisibleBird(birds.first)
           getImages()
-          // setCount(count => count + 1)
 
         }
       } catch(error) {
         console.log(error)
       }
   }
-  // console.log(count)
 
   useEffect(() => {
     for(let i = 0; i < 2; i++) {
       findBird(bird => birds.enqueue(bird))
-    }
-    // setBirdsList(birds)
 
+    }
   }, [])
 
   async function getFirstImage() {
@@ -65,18 +66,6 @@ export default function App() {
   }
 
   async function getImages() {
-    // if(birdsList.length === 1) {
-    //   console.log("getting first bird image")
-    //   try {
-    //     const secondBirdResponse = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_SEARCH_API_KEY}&cx=e2e6b1e2c14314732&searchType=image&q=${birds.first.data.name}`)
-  
-    //     const images = await secondBirdResponse.json()
-    //     birds.first.data.images[0] = images.items[0].link
-    //     setBirdsList(birds)
-    //   } catch (error) {
-    //     console.log(error)
-    //   }
-    // } else {
       console.log("getting next bird image")
       try {
         const secondBirdResponse = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_SEARCH_API_KEY}&cx=e2e6b1e2c14314732&searchType=image&q=${birds.first.next.data.name}`)
@@ -89,10 +78,7 @@ export default function App() {
       } catch (error) {
         console.log(error)
       }
-    // }
   }
-
-
 
   function handleNextBird() {
     console.log("fetching next bird")
